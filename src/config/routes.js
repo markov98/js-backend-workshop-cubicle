c448858450f7e5e589d17891a032b7e6d7ea167f
markov98 <74935630+markov98@users.noreply.github.com>
2023-09-25 11:06:48 +0300

    added 404 page

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 
 router.get('/', (req, res) => {
     res.render("index");
-})
+});
+
+router.get('*', (req, res) => {
+    res.render("404");
+});
 
 module.exports = (app) => {
     app.use(router);
