9e8c886f2b16474e78b5797e1408ff485bde3334
markov98 <74935630+markov98@users.noreply.github.com>
2023-09-26 15:07:01 +0300

    home page now correctly displays stored cubes

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -5,7 +5,7 @@ const cubes = [
         id: 'nwl33cln05tdp4',
         name: 'cube1',
         description: 'testing',
-        imageUrl: '/details/5c3a6888f78d6638ec8fdad6',
+        imageUrl: '/test/123',
         difficultyLevel: 2
       }
 ];
