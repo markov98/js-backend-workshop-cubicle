90af4308ff8e606536a9521dda5b6d33bae60da9
markov98 <74935630+markov98@users.noreply.github.com>
2023-09-26 14:49:16 +0300

    added default cube, added cubeTemplate.hbs

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,6 +1,14 @@
 const uniqid = require('uniqid');
 
-const cubes = [];
+const cubes = [
+    {
+        id: 'nwl33cln05tdp4',
+        name: 'cube1',
+        description: 'testing',
+        imageUrl: '/details/5c3a6888f78d6638ec8fdad6',
+        difficultyLevel: 2
+      }
+];
 
 exports.create = (cubeData) => {
     const newCube = {
@@ -9,7 +17,7 @@ exports.create = (cubeData) => {
     };
 
     cubes.push(newCube);
-    
+
     return newCube;
 }
 
