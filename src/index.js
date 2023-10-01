117e66cda15c620cc647caf8a0faf9164d055470
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-01 22:10:21 +0300

    rewriting index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const {PORT, DBURL} = require('./config/constConfig');
 const app = require('express')();
 
+const {PORT, DBURL} = require('./config/constConfig');
+
+// Initializing app
 require('./config/expressConfig')(app);
-require('./config/dbConfig')(DBURL);
 require('./config/routes')(app);
+
+// Connecting to DB
+require('./config/dbConfig')(DBURL)
+    .then(() => {console.log('Connected to DB!')})
+    .catch((err) => console.log(err));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
