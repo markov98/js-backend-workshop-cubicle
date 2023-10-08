3a6dfa01c4fc43a4d1663372fd2bac518bb30b2c
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-08 22:31:31 +0300

    added userController

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeContrl = require('../controllers/homeController.js');
 const cubeContrl = require('../controllers/cubeController.js');
 const accesrContrl = require('../controllers/accessoryController.js');
+const userContrrl = require('../controllers/userController.js');
 
 router.use(homeContrl);
 router.use('/cubes', cubeContrl);
 router.use('/accessories', accesrContrl);
+router.use('/users', userContrrl);
 
 router.get('*', (req, res) => {
     res.redirect('/404')
