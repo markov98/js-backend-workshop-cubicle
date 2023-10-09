3db90ae13d57e74b1afb645ce7ec0e250e3c98ae
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-09 21:59:17 +0300

    changed arrow function to regular function to fix a bug, added encryption to the password

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
 
-userSchema.virtual('repeatPassword').set(value => {
+userSchema.virtual('repeatPassword').set(function (value) {
     if (value !== this.password) {
         throw new mongoose.MongooseError('Passwords do not match!');
     }
+});
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 });
 
 const User = mongoose.model('User', userSchema);
