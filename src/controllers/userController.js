f93edb1ebcceee67172461f995fc33c762ecc78d
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-09 18:58:14 +0300

    added user service, fixed form actions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,11 @@ router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/users/login');
+});
+
 router.get('/login', (req, res) => {
     res.render('user/login');
 });
