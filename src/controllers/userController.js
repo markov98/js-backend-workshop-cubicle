fb489b9508760621273b792b33662ba81ad649db
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-09 19:24:44 +0300

    initialized User model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const userService = require('../services/userService');
 
 router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
+    await userService.register(username, password, repeatPassword);
     res.redirect('/users/login');
 });
 
