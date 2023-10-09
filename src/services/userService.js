111df802d35dda7aefd8dd2987df823242e3b4da
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-09 23:15:18 +0300

    simplifying username and password validation

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,17 +4,11 @@ const bcrypt = require('bcrypt');
 exports.register = (username, password, repeatPassword) => User.create({username, password, repeatPassword});
 
 exports.login = async (username, password) => {
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
 
-    if (!user) {
-       throw new Error('Invalid username or password!');
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        throw new Error('Invalid username or password!');
     }
 
-    const isValid = await bcrypt.compare(password, user.password);
-
-    if (!isValid) {
-        throw new Error('Invalid username or password!');
-     }
-
     return user;
-}
+};
