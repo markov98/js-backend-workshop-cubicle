b372f822eb475dfa40132a636d24616be802051b
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-10 14:35:22 +0300

    expanded auth middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,19 @@
 const { jwt } = require('../librery/jwt');
 const { SECRET } = require('../config/constConfig');
 
-exports.auth = {
-    
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, SECRET);
+            req.user = decodedToken;
+        } catch (err) {
+            console.log(err);
+            res.cookieClear('auth');
+            res.redirect('/users/login');
+        }
+    } else {
+        next();
+    }
 }
