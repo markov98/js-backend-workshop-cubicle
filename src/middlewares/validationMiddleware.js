34e98e2d178c86a84e088bc14d063cf7bc37cc22
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-14 01:41:38 +0300

    updated validation middleware

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -1,14 +1,14 @@
 exports.isPasswordValid = (req, res, next) => {
-    if (!req.body.password || req.body.password.length < 6) {
-        res.send('Password must be at least 6 chars long.');
+    if (!req.body.password || req.body.password.length < 8) {
+        res.send('Password must be at least 8 chars long.');
     } else {
         next();
     }
 }
 
 exports.isUsernameValid = (req, res, next) => {
-    if (!req.body.username || req.body.username.length < 6) {
-        res.send('Username must be at least 6 chars long.');
+    if (!req.body.username || req.body.username.length < 5) {
+        res.send('Username must be at least 5 chars long.');
     } else {
         next();
     }
